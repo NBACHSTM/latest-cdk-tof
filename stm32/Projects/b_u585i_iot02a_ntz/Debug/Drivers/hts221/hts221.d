5a11d8762bcd161b0f690900d385bfb22552501a
NBACHSTM <nacim.bachiri@st.com>
2024-05-28 12:12:16 +0000

    deleted extra files

--- stm32/Projects/b_u585i_iot02a_ntz/Debug/Drivers/hts221/hts221.d
+++
@@ -1,6 +0,0 @@
-Drivers/hts221/hts221.o: \
- C:/Users/bachirin/Desktop/AWS_gs/aws_poc_aed/Drivers/BSP/Components/hts221/hts221.c \
- C:/Users/bachirin/Desktop/AWS_gs/aws_poc_aed/Drivers/BSP/Components/hts221/hts221.h \
- C:/Users/bachirin/Desktop/AWS_gs/aws_poc_aed/Drivers/BSP/Components/hts221/hts221_reg.h
-C:/Users/bachirin/Desktop/AWS_gs/aws_poc_aed/Drivers/BSP/Components/hts221/hts221.h:
-C:/Users/bachirin/Desktop/AWS_gs/aws_poc_aed/Drivers/BSP/Components/hts221/hts221_reg.h:
--- stm32/Projects/b_u585i_iot02a_ntz/Debug/Drivers/ism330dhcx/ism330dhcx_reg.d
+++
@@ -1,4 +0,0 @@
-Drivers/ism330dhcx/ism330dhcx_reg.o: \
- C:/Users/bachirin/Desktop/AWS_gs/aws_poc_aed/Drivers/BSP/Components/ism330dhcx/ism330dhcx_reg.c \
- C:/Users/bachirin/Desktop/AWS_gs/aws_poc_aed/Drivers/BSP/Components/ism330dhcx/ism330dhcx_reg.h
-C:/Users/bachirin/Desktop/AWS_gs/aws_poc_aed/Drivers/BSP/Components/ism330dhcx/ism330dhcx_reg.h:
