83016239a08c99af5ec7e47bfd99b91adf429023
NBACHSTM <nacim.bachiri@st.com>
2024-08-06 14:52:41 +0000

    first commit cdkV2

--- mlops/pipelines/stm/stm32ai-modelzoo/evaluate.py
+++ mlops/pipelines/stm/stm32ai-modelzoo/evaluate.py
@@ -7,12 +7,12 @@ import traceback
 
 if __name__ == "__main__":
     
-    module_path = os.path.abspath('hand_posture/scripts/evaluate')
+    module_path = os.path.abspath('hand_posture/src/')
     if module_path not in sys.path:
         sys.path.append(module_path)
         
     try:
-        subprocess.run(["python", os.path.join(module_path, "evaluate.py")])
+        subprocess.run(["python", os.path.join(module_path, "stm32ai_main.py operation_mode='evaluation'")])
         print('Evaluation complete.')
 
             # A zero exit code causes the job to be marked a Succeeded.
