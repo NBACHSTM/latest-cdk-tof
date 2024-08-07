b45abd04fe4cf8d50431bab7e20fd1d4e9fe7b04
NBACHSTM <nacim.bachiri@st.com>
2024-08-07 16:18:01 +0000

    changed subprocess.run in evaluate.py 2

--- mlops/pipelines/stm/stm32ai-modelzoo/evaluate.py
+++ mlops/pipelines/stm/stm32ai-modelzoo/evaluate.py
@@ -15,8 +15,7 @@ if __name__ == "__main__":
         subprocess.run([
         "python",
         os.path.join(module_path, "stm32ai_main.py"),
-        "--config-path", "./config_file_examples/",
-        "--config-name", "evaluation_config.yaml"
+        "operation_mode='evaluation'"
         ])
         
         print('Evaluation complete.')
